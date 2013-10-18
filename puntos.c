f440e2a815206fc16a89708f2764de4b9a66e429
usuario <d.lozano343@uniandes.edu.co>
2013-10-17 19:50:41 -0500

    datos iniciales, toca corregir las velocidades

--- puntos.c
+++ puntos.c
@@ -17,7 +17,7 @@ void main(int argc,char **argv){
   y=atof(argv[2]);
   Vpx=atof(argv[3]);
   Vpy=atof(argv[4]);
-  
+  int cuerpos=120;
   float pi=atan(1)*4;
   float teta1=2*pi/12;
   float teta2=2*pi/18;
@@ -29,43 +29,46 @@ void main(int argc,char **argv){
   float G=1;
   float velocidad=pow(masa*G/radio,1/2);
   float angulo1,angulo2,angulo3,angulo4,angulo5;
+  printf("variables guardadas\n");
+  file=fopen("datos_iniciales.txt","w");
 
-  file=fopen("datos iniciales","w");
-
-  for(i=-1;i<100;i++){
+  for(i=-1;i<cuerpos;i++){
     
-    
-    if(i=-1){
+  
+    if(i==-1){//centro de la galaxia
       //centro
-      fprintf(file,"%d %lf %lf %lf ",-1 ,x,y,Vpx,Vpy);
-    }
-    
-    if(i>-1 && i<=11){
-      //12
+      fprintf(file,"%d %lf %lf %lf %f\n",i ,x,y,Vpx,Vpy);
       
+    }
+    // fprintf(file,"%d\n",i);
+    if(i>-1 && i<=11){//12
+     
       angulo1=teta1*(12-i);
-      fprintf(file,"%d %lf %lf %lf %lf",i,radio*cos(angulo1)+x,1*radio*sin(angulo1)+y,Vpx+velocidad*cos(angulo1),Vpy+velocidad*sin(angulo1));
+      fprintf(file,"%d %lf %lf %lf %lf\n",i,radio*cos(angulo1)+x,1*radio*sin(angulo1)+y,Vpx+velocidad*cos(angulo1),Vpy+velocidad*sin(angulo1));
+      //  fprintf(file,"%d\n",i);
     }
 
     if(i>11 && i<=29){//18
       angulo2=teta2*(29-i);
-      fprintf(file,"%d %lf %lf %lf %lf",i,radio*cos(angulo2)+x,2*radio*sin(angulo2)+y,Vpx+velocidad*cos(angulo2),Vpy+velocidad*sin(angulo2));
+      fprintf(file,"%d %lf %lf %lf %lf\n",i,2*radio*cos(angulo2)+x,2*radio*sin(angulo2)+y,Vpx+pow(1/2,1/2)*velocidad*cos(angulo2),Vpy+velocidad*pow(1/2,1/2)*sin(angulo2));
     }
 
     if(i>29 && i<=53){//24
       angulo3=teta3*(53-i);
-      fprintf(file,"%d %lf %lf %lf %lf",i,radio*cos(angulo3)+x,3*radio*sin(angulo3)+y,Vpx+velocidad*cos(angulo3),Vpy+velocidad*sin(angulo3));
+      fprintf(file,"%d %lf %lf %lf %lf\n",i,3*radio*cos(angulo3)+x,3*radio*sin(angulo3)+y,Vpx+velocidad*cos(angulo3)*pow(1/3,1/2),Vpy+velocidad*sin(angulo3)*pow(1/3,1/2));
     }
 
      if(i>53 && i<=83){//30
        angulo4=teta4*(83-i);
-      fprintf(file,"%d %lf %lf %lf %lf",i,radio*cos(angulo4)+x,4*radio*sin(angulo4)+y,Vpx+velocidad*cos(angulo4),Vpy+velocidad*sin(angulo4));
+      fprintf(file,"%d %lf %lf %lf %lf\n",i,4*radio*cos(angulo4)+x,4*radio*sin(angulo4)+y,Vpx+velocidad*cos(angulo4)*pow(1/4,1/2),Vpy+velocidad*sin(angulo4)*pow(1/4,1/2));
     }
 
-     if(i>83 && i<=99){//36
+     if(i>83 && i<=cuerpos-1){//36
        angulo5=teta5*(99-i);
-      fprintf(file,"%d %lf %lf %lf %lf",i,radio*cos(angulo5)+x,5*radio*sin(angulo5)+y,Vpx+velocidad*cos(angulo5),Vpy+velocidad*sin(angulo5));
+       fprintf(file,"%d %lf %lf %lf %lf %d\n",i,5*radio*cos(angulo5)+x,5*radio*sin(angulo5)+y,Vpx+velocidad*cos(angulo5)*pow(1/5,1/2),Vpy+velocidad*sin(angulo5)*pow(1/5,1/2));
     }  
 
+
+  }
 
 }
