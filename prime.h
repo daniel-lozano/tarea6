829df358ac4b71a9c33ceb9e27e1a4f4fb0aafef
usuario <d.lozano343@uniandes.edu.co>
2013-11-05 11:10:29 -0500

    se borraron los archivos de mas

--- prime.h
+++
@@ -1,4 +0,0 @@
-float primex1(float x,float xp,float tiempo,float y,float X,float Y);
-float primex2(float x,float xp,float tiempo,float y,float X,float Y);
-float primey1(float y,float yp,float tiempo,float x,float X,float Y);
-float primey2(float y,float yp,float tiempo,float x,float X,float Y);
--- runge.h
+++
@@ -1,3 +0,0 @@
-float* runge1(float *fp1,float *fp2,float *tiempo,float paso, int indice,int i,float dato,float X,float Y);
-
-float* runge2(float *fp1,float *fp2,float *tiempo,float paso, int indice,int i,float dato,float X,float Y);
